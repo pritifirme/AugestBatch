7b7a583e182fc6f1d8a7d1231992499a3403144e
pritifirame <pritifirame@gmail.com>
2023-12-26 16:27:22 +0530

    New annotation added in home page test

--- src/test/java/Test/HomePageTest.java
+++ src/test/java/Test/HomePageTest.java
@@ -1,5 +1,15 @@
 package Test;
 
+import org.testng.annotations.BeforeMethod;
+
+import pojo.Browser;
+
 public class HomePageTest extends BaseTest {
+	@BeforeMethod
+	public void OpenBrowser()
+	{
+		driver=Browser.openchrome("https://www.facebook.com/");
+		
+	}
 
 }
